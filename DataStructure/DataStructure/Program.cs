0653dea0d83e60d8e1d131a01ac0af1d8f57e301
Piyushnmj <piyushnmj@yahoo.com>
2022-11-17 15:24:38 +0530

    Welcome

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table");
+            Console.ReadLine();
+        }
+    }
+}
