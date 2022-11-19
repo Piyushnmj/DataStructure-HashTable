980bf1d5f371a29db64303109df3f46819d20dfb
Piyushnmj <piyushnmj@yahoo.com>
2022-11-19 14:27:49 +0530

    Removing a word from paragraph

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -32,15 +32,23 @@
                 Console.WriteLine($"{i} index value : {hash.Get(i)}");
             }
 
-            Console.WriteLine("\nFrequency of Words ==> ");
+            //Console.WriteLine("\nFrequency of Words ==> ");
 
-            string[] arr = new string[hash.size];
+            //string[] arr = new string[hash.size];
+            //for (int i = 0; i < hash.size; i++)
+            //{
+            //    arr[i] = hash.Get(i);
+            //}
+
+            //hash.FreqOfWords(arr, arr.Length);
+
+            Console.WriteLine("\nRemoving a Word from Para ==>");
+            hash.Remove(17);
+
             for (int i = 0; i < hash.size; i++)
             {
-                arr[i] = hash.Get(i);
+                Console.WriteLine($"{i} index value : {hash.Get(i)}");
             }
-
-            hash.FreqOfWords(arr, arr.Length);
 
             Console.ReadLine();
         }
